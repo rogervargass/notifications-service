c67e7a554d079a9e52a90ba35a51ce20eb2de3e7
Roger Vargas <roger.vargas@cwi.com.br>
2022-12-18 19:46:46 -0300

    feat: add use case get notifications with tests

--- src/app/use-cases/get-recipient-notifications.spec.ts
+++ src/app/use-cases/get-recipient-notifications.spec.ts
@@ -1,11 +1,11 @@
 import { makeNotification } from '@test/factories/notification-factory';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
-import { CountRecipientNotification } from './count-recipient-notification';
+import { GetRecipientNotification } from './get-recipient-notifications';
 
-describe('Count recipients notifications', () => {
-  it('should be able to count recipient notifications', async () => {
+describe('Get recipients notifications', () => {
+  it('should be able to get recipient notifications', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
-    const countRecipientNotification = new CountRecipientNotification(
+    const getRecipientNotification = new GetRecipientNotification(
       notificationsRepository,
     );
 
@@ -21,10 +21,16 @@ describe('Count recipients notifications', () => {
       makeNotification({ recipientId: 'recipient-2' }),
     );
 
-    const { count } = await countRecipientNotification.execute({
+    const { notifications } = await getRecipientNotification.execute({
       recipientId: 'recipient-1',
     });
 
-    expect(count).toEqual(2);
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+        expect.objectContaining({ recipientId: 'recipient-1' }),
+      ]),
+    );
   });
 });
